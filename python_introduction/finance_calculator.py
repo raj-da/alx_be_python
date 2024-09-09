b34f4e5a166e0773a6720af368ec662cdcc046e4
raj-da <rajafgudeta@gmail.com>
2024-09-09 19:50:07 +0300

    changed m_ into monthly in every variable contaning it

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-m_income = float(input("Enter your monthly income: "))
-m_expenses = float(input("Enter your total monthly expenses: "))
-m_savings = m_income - m_expenses
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 
-print(f"Your monthly savings are {m_savings}.")
-print(f"projected savings after one year, with interest, is: {m_savings * 12 + (m_savings * 12 * 0.05)}")
+print(f"Your monthly savings are {monthly_savings}.")
+print(f"projected savings after one year, with interest, is: {monthly_savings * 12 + (monthly_savings * 12 * 0.05)}")
