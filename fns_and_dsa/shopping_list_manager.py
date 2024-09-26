dedc4250e1138f47fd0f4d5651876c636c2e8736
raj-da <rajafgudeta@gmail.com>
2024-09-26 17:19:36 +0300

    changed the prompt messege for choice 1 and 2

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,11 +13,11 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            add = input('Enter item to be added: ')
+            add = input('Enter the item to add: ')
             shopping_list.append(add)
         elif choice == '2':
             # Prompt for and remove an item
-            remove = input('Enter item to be removed: ')
+            remove = input('Enter the item to remove: ')
             shopping_list.remove(remove)
         elif choice == '3':
             # Display the shopping list
