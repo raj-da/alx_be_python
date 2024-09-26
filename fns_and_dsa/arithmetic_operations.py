391bbf05139903b8d9dc1dd91ba3b0e52609733b
raj-da <rajafgudeta@gmail.com>
2024-09-26 17:03:09 +0300

    Added an else for division

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,7 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 return 'Division by 0 is undefined'
-            return num1 / num2
+            else:
+                return num1 / num2
         case _:
             'Error! Unrecognized operation'
