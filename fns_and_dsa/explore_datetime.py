5ad0ac6a19e66b555d2d6c62fab9e8d070990c58
raj-da <rajafgudeta@gmail.com>
2024-09-26 19:30:42 +0300

    removed if __name__ condition

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,13 +4,13 @@ def display_current_datetime():
     current_date = datetime.datetime.now()
     return current_date
 
-# print(display_current_datetime())
 
 def calculate_future_date(number_of_days):
     t_delta = datetime.timedelta(days=number_of_days)
     future_date = datetime.date.today() + t_delta
 
     return future_date
+
 
 def main():
     print(f'Current date and time: {display_current_datetime()}')
@@ -19,5 +19,4 @@ def main():
     print(f'Future date: {calculate_future_date(number_of_days)}')
 
 
-if __name__ == '__main__':
-    main()
+main()
