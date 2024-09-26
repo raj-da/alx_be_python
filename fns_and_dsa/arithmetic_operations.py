67acc7d1d5c5cd13a664d96188216673c6d72f4c
raj-da <rajafgudeta@gmail.com>
2024-09-26 17:01:17 +0300

    Added division operation on case divide in arithmetic_operations.py file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,5 +9,6 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 return 'Division by 0 is undefined'
+            return num1 / num2
         case _:
             'Error! Unrecognized operation'
