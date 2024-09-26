5fb3aba75c8f9d71ae0d2cd412428f9e70ff8aee
raj-da <rajafgudeta@gmail.com>
2024-09-26 16:58:44 +0300

    created fns_and_dsa folder and arithmetic_operations.py file in it

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num2 == 0:
+                return 'Division by 0 is undefined'
+        case _:
+            'Error! Unrecognized operation'
