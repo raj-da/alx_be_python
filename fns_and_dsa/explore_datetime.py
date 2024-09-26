97e9c77a5bd4458afaaeaaf86e98cd2f5994a12b
raj-da <rajafgudeta@gmail.com>
2024-09-26 19:41:03 +0300

    removed the microseconds from current_date 2

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,22 +1,19 @@
 from datetime import datetime, timedelta
 
 def display_current_datetime():
-    current_date = datetime.now().replace(microsecond=0)
+    # Get the current date and time, and format it as "YYYY-MM-DD HH:MM:SS"
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return current_date
-
 
 def calculate_future_date(number_of_days):
     t_delta = timedelta(days=number_of_days)
     future_date = datetime.today().date() + t_delta
-
     return future_date
-
 
 def main():
     print(f'Current date and time: {display_current_datetime()}')
 
-    number_of_days = int(input('Enter the number of days to add to the current date: '))
+    number_of_days = int(input('Enter the number of days to calculate future date: '))
     print(f'Future date: {calculate_future_date(number_of_days)}')
-
 
 main()
