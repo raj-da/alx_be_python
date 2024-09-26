6a4b93d86234fdffab71ea23492c96a3df5f615c
raj-da <rajafgudeta@gmail.com>
2024-09-26 19:39:34 +0300

    removed the microseconds from current_date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 def display_current_datetime():
-    current_date = datetime.now()
+    current_date = datetime.now().replace(microsecond=0)
     return current_date
 
 
