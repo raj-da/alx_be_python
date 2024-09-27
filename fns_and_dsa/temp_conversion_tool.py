c6091e3867ddb141d44d077e8a5ad9510a94b0d8
raj-da <rajafgudeta@gmail.com>
2024-09-27 15:04:44 +0300

    minimized the code in the conversion functions

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,16 +2,10 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    global FAHRENHEIT_TO_CELSIUS_FACTOR
-
-    celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
-    return celsius
-
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR 
+    
 def convert_to_fahrenheit(celsius):
-    global CELSIUS_TO_FAHRENHEIT_FACTOR
-
-    fahrenheit = (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
-    return fahrenheit
+    return (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
 
 def main():
     try:
