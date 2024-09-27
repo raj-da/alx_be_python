71e6398e2099fe8a375176db9ad41c5918492a5d
raj-da <rajafgudeta@gmail.com>
2024-09-27 15:18:03 +0300

    update3

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,7 +5,7 @@ def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR 
     
 def convert_to_fahrenheit(celsius):
-    return (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
+    return (CELSIUS_TO_FAHRENHEIT_FACTOR) * celsius + 32
 
 def main():
     try:
