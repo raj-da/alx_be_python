32307991491f4d92831fdb592ee10193962d86a5
raj-da <rajafgudeta@gmail.com>
2024-10-03 21:24:18 +0300

    update2

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,20 +7,26 @@ class TestCalc(unittest.TestCase):
     def setUp(self) -> None:
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(10, 5), 15)
         self.assertEqual(self.calc.add(10, -5), 5)
+        self.assertEqual(self.calc.add(-10, -5), -15)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(10, -5), 15)
+        self.assertEqual(self.calc.subtract(-10, -5), -5)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(10, 5), 50)
         self.assertEqual(self.calc.multiply(10, -5), -50)
+        self.assertEqual(self.calc.multiply(-4, -5), 20)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 5), 2)
         self.assertEqual(self.calc.divide(5, 10), 0.5)
-        self.assertEqual(self.calc.divide(10, 5), None)
+        self.assertEqual(self.calc.divide(10, 5))
+
+    if __name__ == "__main__":
+        unittest.main()
         
