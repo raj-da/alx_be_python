d660b6cb26634a47af2dd6ae58bdaa61f62b7005
raj-da <rajafgudeta@gmail.com>
2024-10-03 18:49:06 +0300

    Added bank_account.py file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, balance=None) -> None:
+        if balance is None:
+            balance = 0
+        self.account_balance = balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+        
+    def display_balance(self):
+        print(f'Current Balance: ${self.account_balance}')
